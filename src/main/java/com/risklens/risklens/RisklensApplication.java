5885a394c63a29b7e6e7873cd74bc8fda365dee4
Priyanka <priyankaapandey.2610@gmail.com>
2026-08-24 15:29:02 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/risklens/risklens/RisklensApplication.java
@@ -0,0 +1,13 @@
+package com.risklens.risklens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisklensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisklensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/risklens/risklens/RisklensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risklens.risklens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisklensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
